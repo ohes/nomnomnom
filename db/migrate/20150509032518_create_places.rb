f493c513bc0302f048bdf0eb6bdf4c69e5aba45f
ohes <estheroh@gmail.com>
2015-05-09 14:38:23 +0000

    listing all the places in database on index page

--- db/migrate/20150509032518_create_places.rb
+++ db/migrate/20150509032518_create_places.rb
@@ -2,11 +2,11 @@ class CreatePlaces < ActiveRecord::Migration
   def change
     create_table :places do |t|
 
-t.string :name
-t.text :description
-t.string :address
+	t.string :name
+	t.text :description
+	t.string :address
 
-      t.timestamps
+    t.timestamps
     end
   end
 end
