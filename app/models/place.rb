977106217acd0e7e1f1af1aba07058eaa0a5e266
ohes <estheroh@gmail.com>
2015-05-26 03:24:47 +0000

    added validations for all fields

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,10 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, length: { 
+		minimum: 3,
+		too_short: "name must have at least 3 characters"
+	}
+	validates :address, :presence => true
+	validates :description, :presence => true
+
 end
